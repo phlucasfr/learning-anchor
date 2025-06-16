72e595616161242bb9ddce88ae917f7b341a90a5
Phelipe <phlucasfr@gmail.com>
2025-06-16 09:43:29 -0300

    Core Concepts - Program Derived Address - Pda Constraints - bump examples

--- first-project/programs/first-project/src/pda_constraints.rs
+++ first-project/programs/first-project/src/pda_constraints.rs
@@ -26,7 +26,23 @@ pub struct InstructionAccountsMultiple<'info> {
     pub signer: Signer<'info>,
     #[account(
         seeds = [b"hello world", signer.key().as_ref()], // Multiple seeds can be specified in the seeds constraint. The seeds constraint can also reference other account addresses or account data.
-        bump,
+        bump, //When using the bump constraint without a value, the bump is automatically calculated each time the instruction is invoked.
     )]
     pub pda_account: SystemAccount<'info>,
+}
+
+// By storing the bump value in the account's data, the program doesn't need to recalculate it, saving compute units.
+// The saved bump value can be stored on the account itself or another account.
+#[derive(Accounts)]
+pub struct InstructionAccountsWithSpecifiedBump<'info> {
+    #[account(
+        seeds = [b"hello world"],
+        bump = pda_account.bump_seed
+    )]
+    pub pda_account: Account<'info, CustomAccount>, // This assumes that the PDA account has been created and the bump seed is stored as a field on an existing account.
+}
+
+#[account]
+pub struct CustomAccount {
+    pub bump_seed: u8,
 }
